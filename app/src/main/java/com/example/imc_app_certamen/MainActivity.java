32ddec372b419c884ea60792dbc99b0842e4b78f
SinChucrut1 <carrasco.emc2@gmail.com>
2024-11-12 14:15:43 -0300

    Creacion Aplicacion

---
+++ app/src/main/java/com/example/imc_app_certamen/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.imc_app_certamen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
